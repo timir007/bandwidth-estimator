21b63d3e86086c336ddcd3f98403f9edb4c4caaa
Mohammad Rajiullah <mohammad.rajiullah@kau.se>
2016-10-18 10:50:15 +0200

    updated the python files to calculate throughput

--- parse_recv_times.py
+++ parse_recv_times.py
@@ -1,8 +1,21 @@
+from __future__ import division
 import sys
 import os
+import linecache
+
+
+
+
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
 
 #What interval should be used on x-axis. I.e., how many ms should one tick
 #express
+# but express interval length in number of packets
 interval_length = int(sys.argv[1])
 logfile = open(sys.argv[2], "r")
 
@@ -13,14 +26,15 @@ initial_tstamp = float(line[0])
 cur_interval_start = initial_tstamp
 
 #Timestamp in file is in seconds
-cur_interval_end = initial_tstamp + (interval_length / 1000.0)
+#cur_interval_end = initial_tstamp + (interval_length / 1000.0)
 
+cur_interval_end= float(linecache.getline(sys.argv[2], int(sys.argv[1])).strip().split(' ')[0])
 #Reset file descriptor
 logfile.seek(0, os.SEEK_SET)
-x_val = 0
 read = True
 interval_bytes = 0
-num_mes = 0
+x_val=0
+num_packets=0
 
 while True:
     #To avoid the additional function, have a check here for read. This is set
@@ -34,22 +48,27 @@ while True:
         line = line.strip().split(' ')
     
     tstamp, numbytes = float(line[0]), int(line[1])
+    num_packets=num_packets+1  
+    
 
-    if tstamp > cur_interval_end:
-        print x_val, interval_bytes
-        x_val += 1
+    if tstamp >= cur_interval_end:
+	x_val=cur_interval_end-cur_interval_start
+        print x_val, sizeof_fmt(interval_bytes), ((interval_bytes*8)/(1024*1024))/x_val 
+		     
         interval_bytes = 0
-        num_mes = 0
-
-        read = False
-        cur_interval_start = cur_interval_end
-        cur_interval_end = cur_interval_start + (interval_length / 1000.0)
+        #read = False
+        if num_packets < (int(sys.argv[1])*int(sys.argv[3])):
+		cur_interval_start= float(linecache.getline(sys.argv[2], interval_length+1).strip().split(' ')[0])
+	interval_length=interval_length+int(sys.argv[1])
+	if num_packets < (int(sys.argv[1])*int(sys.argv[3])):
+		cur_interval_end= float(linecache.getline(sys.argv[2], interval_length).strip().split(' ')[0])
     else:
         #if tstamp > 1357573434.78 and tstamp < 1357573434.88:
         #    print tstamp, cur_interval_start, cur_interval_end
 
         read = True
         interval_bytes += numbytes
-        num_mes += 1
+	#print tstamp, interval_bytes 
+     
 
 logfile.close()
